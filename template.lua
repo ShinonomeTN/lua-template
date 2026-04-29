25e1995f54b1a2f82539f13aa51111b28aa9026a
cattenlinger <cattenlinger@outlook.com>
2026-04-30 04:17:06 +0800

    fix: _ENV missing with Lua 5.2+

--- template.lua
+++ template.lua
@@ -1,13 +1,5 @@
 local template = {}
 
---[[
-`loadstring` was deprecated since 5.2, use `load` instead
-(see: https://www.lua.org/manual/5.2/manual.html#8.2)
-]]--
-local loadstring = loadstring or function(str, chkn)
-  return load(str, chkn or str, 't')
-end
-
 function template.escape(data)
   return tostring(data == nil and "" or data):gsub("[\">/<'&]", {
     ["&"] = "&amp;",
@@ -21,35 +13,47 @@ end
 
 function template.print(data, args, callback)
   local callback = callback or print
-  local function exec(data)
-    if type(data) == "function" then
-      local env = args or {}
-      setmetatable(env, { __index = _G })
-      if _ENV then -- Lua 5.2+
-        local wrapper, err = load([[
-          return function(_ENV, exec, ...)
-            local f = ...
-            f(exec)
-          end
-        ]], "wrapper", "t", env)
-        if not wrapper then
-          error(err)
-        end
-        wrapper()(env, exec, data)
-      else
-        setfenv(data, env)
-        data(exec)
-      end
-    else
-      callback(tostring(data == nil and "" or data))
+  
+  local env = args or {}
+  setmetatable(env, { __index = _G })
+
+  local function exec(ins)
+    if type(ins) ~= "function" then
+      return callback(tostring(ins == nil and "" or ins))
     end
+    -- if type(data) == "function"
+
+    if _ENV then -- Lua 5.2+ uses _ENV
+      local wrapper, err = load(
+        [[return function(_ENV,exec,...) local f=...; f(exec, _ENV); end]],
+        "wrapper", "t", env
+      )
+      if not wrapper then error(err) end
+      wrapper()(env, exec, ins)
+      return
+    end
+
+    -- Lua 5.1
+    setfenv(ins, env)
+    ins(exec)
   end
+
   exec(data)
 end
 
+
+local template_entry do
+  local s = "function(_"
+  if not _ENV
+  then s = s .. ') '
+  else s = s .. ",_ENV) "
+  end
+  template_entry = s
+end
+
 function template.parse(data, minify)
-  local str = 
-    "return function(_)" .. 
+  local str =
+    "return " .. template_entry .. 
       "function __(...)" ..
         "_(require('template').escape(...))" ..
       "end " ..
@@ -63,6 +67,7 @@ function template.parse(data, minify)
         gsub("%?>", " _[=[") ..
       "]=] " ..
     "end"
+
   if minify then
     str = str:
       gsub("^[ %s]*", ""):
@@ -72,11 +77,17 @@ function template.parse(data, minify)
   return str
 end
 
+--[[
+  `loadstring` was deprecated since 5.2, use `load` instead
+  (see: https://www.lua.org/manual/5.2/manual.html#8.2)
+]]--
+local loadstring = loadstring or function(str, chkn)
+  return load(str, chkn or str, 't') -- We will pass _ENV manually
+end
+
 function template.compile(...)
   local f, err = loadstring(template.parse(...))
-  if err then
-    error(err)
-  end
+  if err then error(err); end
   return f()
 end
 
