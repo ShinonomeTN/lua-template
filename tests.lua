020e1483097d31c5250bfea07916f972fb2f7870
cattenlinger <cattenlinger@outlook.com>
2026-04-30 04:32:40 +0800

    Add a minify template test

--- tests.lua
+++ tests.lua
@@ -18,6 +18,13 @@ describe("template module", function()
       assert.equal("Hello, World!", table.concat(output, ""))
     end)
 
+    it("minial template is functional", function()
+      local func = template.compile([[<? for i = 1, max do ?>.<?end?>]], true)
+      local output = {}
+      template.print(func, { max = 10 }, function(s) table.insert(output, s) end)
+      assert.equal("..........", table.concat(output, ""))
+    end)
+
     it("errors on invalid template", function()
       assert.has_error(function()
         template.compile("<%= ) %>")
