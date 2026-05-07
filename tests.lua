0225cf641824f23db8c2e78a5fdb8836b1ee608c
cattenlinger <cattenlinger@outlook.com>
2026-05-07 18:50:46 +0800

    Add bigger template parsing test

--- tests.lua
+++ tests.lua
@@ -38,6 +38,22 @@ describe("template module", function()
       assert.equal("&amp;&lt;&gt;&quot;&#39;&#47;", table.concat(output, ""))
     end)
 
+    it("big template rendering", function()
+      local fd = assert(io.open("./test/dir_page.ltpl", "r"))
+      local content = assert(fd:read("*a"))
+      fd:close()
+      local func = template.compile(content, true)
+      local data = {
+        path="/", 
+        files={ 
+          {css_class="directory"; href="/"; filename="."; size=0; size="0B"; time="1970-01-01 00:00:00"}
+        }
+      }
+      local output={}
+      template.print(func, data, function(s) table.insert(output, s) end)
+      print(table.concat(output))
+    end)
+
     it("errors on invalid template", function()
       assert.has_error(function()
         template.compile("<%= ) %>")
